a3a608deb94af4e0f28d72ebc9bba656fcc054fc
awake416 <chhikara.vivek@gmail.com>
2021-06-27 16:15:08 +0530

    issue-3 - update usage

--- workspaces.tf
+++ workspaces.tf
@@ -1,8 +1,11 @@
-
 locals {
   prefix = var.prefix
-  workspace_name = join("", [local.prefix, "-", var.env])
-  working_directory = join("/", [local.prefix, var.env])
+  workspace_name = join("", [
+    local.prefix,
+    "-",
+    var.env])
+  working_directory = var.working_dir == "" ? "./" : var.working_dir
+  execution_mode = var.env == "dev" ? "local" : "remote"
 }
 
 resource tfe_workspace workspace {
@@ -11,6 +14,11 @@ resource tfe_workspace workspace {
   auto_apply = true
   terraform_version = "0.15.3"
   working_directory = local.working_directory
+  execution_mode = local.execution_mode
+  vcs_repo {
+    identifier = join("/", [var.terraform_organization, var.prefix])
+    oauth_token_id = var.oauth_token_id
+  }
 }
 
 resource tfe_variable aws_access_key_id {
@@ -30,4 +38,3 @@ resource tfe_variable aws_secret_access_key {
   workspace_id = tfe_workspace.workspace.id
   sensitive = true
 }
-
